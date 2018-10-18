1f536934e51608d24c1f718a16b6aa56ba57a444
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-10-18 19:20:19 -0400

    -

--- JS/main.js
+++ JS/main.js
@@ -2,33 +2,34 @@ function test1(){
 	var fName = prompt("Hey what's your name?");
 	var myColour = prompt("Pick a colour," + fName);
 
-if(myColour == "red"){
-	console.log("ha, this guy likes red");
-}
+	if(myColour == "red"){
+		console.log("ha, this guy likes red");
+	}
 
 
-else if(myColour == "green"){
+	else if(myColour == "green"){
 
 	    	console.log("ah, green. He's alright");
-}
-else if(myColour == "yellow"){
+	}
+	else if(myColour == "yellow"){
+		console.log("ah, yellow. That's trash");
+	}
 
-	    	console.log("ah, yellow. That's trash");
 }
 
 
 function test2(){
 
 	if(age < 14){
-		alert("That's not too old")
+		alert("That's not too old");
 	}
 	else if(age == 14){
-		alert("14 is a good age")
+		alert("14 is a good age");
 	}
 	else if(age > 14){
-		alert("What??")
+		alert("What??");
 	}
 	else{
-		alert("Not a valid ")
+		alert("Not a valid ");
 	}
 }
