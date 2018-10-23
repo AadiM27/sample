a61f29092ef9203059bc25c5b1194f6981962a7a
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-10-23 12:16:59 -0400

    .

--- JS/main.js
+++ JS/main.js
@@ -2,13 +2,13 @@ function test1(){
 	var fName = prompt("Hey what's your name?");
 	var myColour = prompt("Pick a colour, + fName");
 
-	if(myColour == "red"){
+	if(myColour == red){
 		console.log("ha, this guy likes red");
 	}
-	else if(myColour == "green"){
+	else if(myColour == green){
         console.log("ah, green. He's alright");
 	}
-	else if(myColour == "yellow"){
+	else if(myColour == yellow){
 		console.log("ah, yellow. That's trash");
 	}
 }
