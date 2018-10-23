5c3b1609f0c21821eacde463a0d0f5a53e3b1222
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-10-23 12:30:19 -0400

    .

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,41 @@
 function test1(){
-	var fName = prompt("Hey what's your name?");
-	var myColour = prompt("Pick a colour, " + fName);
 
-	if(myColour == "red"){
-		console.log("ha, this guy likes red");
-	}
-	else if(myColour == "green"){
+    var fName = prompt("Hey what's your name?");
+    var myColour = prompt("Pick a colour, " + fName);
 
-	    	console.log("ah, green. He's alright");
-	}
-	else if(myColour == "yellow"){
-		console.log("ah, yellow. That's trash");
-	}
+    if(myColour == "red"){
+
+        console.log("ha. this guy likes red");
+    } 
+    else if(myColour == "green"){
+        console.log("ah, green. He's alright.");
+    }
+    else if(myColour == "yellow"){
+        console.log("het him something yellow.");
+    }
+    else {
+        console.log("that's not a color");
+    }
 }
 
-
 function test2(){
-	var age = prompt("How old are you? And don't worry, I'm not a creep.")
+    var age = prompt("how old are you?");
 
-	if(age < 14){
-		alert("That's not too old");
-	}
-	else if(age == 14){
-		alert("14 is a good age");
-	}
-	else if(age > 14){
-		alert("What??");
-	}
-	else{
-		alert("Not a valid ");
-	}
+
+    if(age < 14){
+        alert("GET OUUUUTTT");
+
+    }
+    else if(age == 14){
+        alert("14 is a good age");
+
+    }
+    else if(age > 14){
+        alert("YOU ARE IIIINNNN");
+
+    }
+    else{
+        alert("Not a valid age");
+    }
 }
 
