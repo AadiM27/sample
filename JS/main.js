f40041bdf2b71b179b609bce1002c84079b3c45d
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-10-23 12:24:34 -0400

    .

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 function test1(){
-	var fName = prompt("Hey what's your name?")
-	var myColour = prompt("Pick a colour," + fName)
+	var fName = prompt("Hey what's your name?");
+	var myColour = prompt("Pick a colour, " + fName);
 
 	if(myColour == "red"){
 		console.log("ha, this guy likes red");
 	}
 	else if(myColour == "green"){
-        console.log("ah, green. He's alright");
+
+	    	console.log("ah, green. He's alright");
 	}
 	else if(myColour == "yellow"){
 		console.log("ah, yellow. That's trash");
@@ -27,7 +28,7 @@ function test2(){
 		alert("What??");
 	}
 	else{
-		alert("Not a valid answer");
+		alert("Not a valid ");
 	}
 }
 
