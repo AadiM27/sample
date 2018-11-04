9ee1ffb83ed20915b5bb7b65c06548b51796c30a
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-11-04 13:44:09 -0500

    .

--- JS/Assignment4.js
+++ JS/Assignment4.js
@@ -1,23 +1,23 @@
 function test3(){
-for ( var i = 1; i <= 100; i++ ){
+	for ( var i = 1; i <= 100; i++ ){
 
-  if ( i%3 === 0 && i%5 === 0 ){
-    console.log("FizzBuzz" );
+	if ( i%3 === 0 && i%5 === 0 ){
+		console.log("FizzBuzz" );
   }
 
-  else if ( i%3 === 0 ){
-    console.log(" Fizz" );
+	else if ( i%3 === 0 ){
+		console.log("Fizz" );
   }
 
-  else if ( i%5 === 0 ){
-    console.log(" Buzz" );
+  	else if ( i%5 === 0 ){
+  		console.log("Buzz" );
   }
 
-  else{
-  	console.log(i);
+ 	else{
+ 		console.log(i);
   }
-
 }
 
 }
 
+test3()
