beb35c9c0efd23f88978fbc5371267d56a65b599
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-11-04 20:04:12 -0500

    .

--- JS/Assignment4.js
+++ JS/Assignment4.js
@@ -2,27 +2,27 @@ function test1(){
   for (var i=1; i<=100; i++) {
 
     if (i%15===0) {
-        console.log("FizzBuzz");
+        alert("FizzBuzz");
     } else if (i%3===0) {
-        console.log("Fizz");
+        alert("Fizz");
     } else if (i%5===0) {
-        console.log("Buzz");
+        alert("Buzz");
     } else {
-        console.log(i)
+        alert(i)
     }
   }
 }
 
 function test2(){
   var x,y,chr;
-  for(x=1; x <=6; x++)
+   for(x=1; x <=6; x++)
   {
 
    for (y=1; y < x; y++)
   {
     chr=chr+("*");        
   }
-    console.log(chr);
+    alert(chr);
     chr='';    
   }
 }
