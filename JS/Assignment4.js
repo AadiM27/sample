eb655e8f876ec363197d80662704d163e849ab0a
AadiM27 <43042266+AadiM27@users.noreply.github.com>
2018-11-18 15:06:24 -0500

    .

--- JS/Assignment4.js
+++ JS/Assignment4.js
@@ -1,20 +1,27 @@
  function test1(){
-  for (var i=1; i<=100; i++) {
+  var i;
+    i = window.prompt ("Insert an integer for Fizzbuzz", "0")
 
     if (i%15===0) {
-        console.log("FizzBuzz");
-    } else if (i%3===0) {
-        console.log("Fizz");
-    } else if (i%5===0) {
-        console.log("Buzz");
+        document.getElementById("fizz").innerHTML = ("FizzBuzz");
+        document.getElementById("buzz").innerHTML = ("FizzBuzz");
+    } 
+    else if (i%3===0) {
+        document.getElementById("fizz").innerHTML = ("FizzBuzz");
+        document.getElementById("buzz").innerHTML = ("Fizz");
+    } 
+    else if (i%5===0) {
+        document.getElementById("fizz").innerHTML = ("FizzBuzz");
+        document.getElementById("buzz").innerHTML = ("Buzz");
     } else {
-        console.log(i)
-    }
-  }
+        document.getElementById("fizz").innerHTML = ("FizzBuzz");
+        document.getElementById("buzz").innerHTML = ("None");
+    }  
 }
 
 function test2(){
   var x,y,chr;
+  x,y,chr = prompt ("Insert an integer. Check the console for a results",)
    for(x=1; x <=6; x++)
   {
 
